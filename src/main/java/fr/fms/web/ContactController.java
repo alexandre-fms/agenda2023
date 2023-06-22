11b962a3c0996db298ec0004b373e6f0770cd433
alexandre-fms <alexandre.arenas@fms-ea.com>
2023-06-22 10:22:18 +0200

    last commit malheureusement

--- src/main/java/fr/fms/web/ContactController.java
+++ src/main/java/fr/fms/web/ContactController.java
@@ -19,6 +19,14 @@ public class ContactController {
     @Autowired
     IntBusinessImpl businessImpl;
 
+    /**
+     * Méthode en GET correspondant à l'url .../index (page d'accueil)
+     * @param model sert à ajouter des éléments partagés avec la vue
+     * @param page correspond à la page active côté front, cela assure la pagination
+     * @param kw est un mot dont on souhaite afficher tous les articles le contenant
+     * @param idCat est l'identifiant de la catégorie dont on souhaite afficher tous les articles / par défaut vaut 0
+     * @return la page articles.html
+     */
     @GetMapping("/index")
     public String index(Model model, @RequestParam(name="page" , defaultValue = "0") int page,
                         @RequestParam(name="keyword" , defaultValue = "") String kw,
@@ -53,28 +61,35 @@ public class ContactController {
         }
         catch(Exception e) {
             model.addAttribute("error",e.getMessage());
-            //logger.error("[ARTICLE CONTROLLER : INDEX] : {} " , e.getMessage());
         }
-
-
-
-
         return "Contacts";
     }
 
-
+    /**
+     * Méthode en GET correspondant à l'url .../delete permettant de supprimer un article à partir de son id
+     * @param id de l'article
+     * @param page la page active
+     * @param keyword le mot clé s'il existe
+     * @param idCat l'identifiant de la catégorie
+     * @param redirectAttrs sert à injecter un paramètre dans la redirection, delors ../index pourra le récupérer via le modelAttribute
+     * @return une redirection vers ../index avec les éléments permettant de garder le contexte actuel
+     */
     @GetMapping("/delete")
     public String delete(Long id, int page, String keyword , Long idCat, RedirectAttributes redirectAttrs) {
         try {
             businessImpl.deleteContact(id);
         } catch (Exception e) {
             redirectAttrs.addAttribute("error",e.getMessage());
-            //logger.error("[ARTICLE CONTROLLER : DELETE] : {} " , e.getMessage());
         }
         return "redirect:/index?page="+page+"&keyword="+keyword + "&idCat=" + idCat;
     }
 
-
+    /**
+     * Méthode en GET correspondant à l'url .../edit permettant d'afficher un article en vue de sa mise à jour
+     * @param id de l'article
+     * @param model sert à ajouter des éléments partagés avec la vue
+     * @return renvoie vers la page .../edit du contact à modifier
+     */
     @GetMapping("/edit")
     public String edit(Long id, Model model) {
         Contact contact;
@@ -84,11 +99,18 @@ public class ContactController {
             model.addAttribute("contact", contact);
         } catch (Exception e) {
             model.addAttribute("error",e.getMessage());
-            //logger.error("[ARTICLE CONTROLLER : EDIT] : {} " , e.getMessage());
         }
         return "edit";
     }
 
+    /**
+     * Méthode en POST permettant l'enregistrement des modifications ou de la création de contacts
+     * @param contact l'objet contact à enregistrer
+     * @param bindingResult sert à la gestion des problèmes de saisie non conforme
+     * @param model
+     * @param redirectAttrs
+     * @return une fois le contact sauvegardé sans erreur, redirection sur la page d'accueil (../index)
+     */
     @PostMapping("/save")
     public String save(@Valid Contact contact, BindingResult bindingResult, Model model, RedirectAttributes redirectAttrs) {
         try {
@@ -100,12 +122,15 @@ public class ContactController {
         }
         catch(Exception e) {
             redirectAttrs.addAttribute("error",e.getMessage());
-            //logger.error("[ARTICLE CONTROLLER : SAVE ARTICLE] : {} " , e.getMessage());
         }
         return "redirect:/index";
     }
 
-
+    /**
+     * Méthode en Get redirigeant vers l'url ../contact permettant de créer un nouveau contact.
+     * @param model
+     * @return renvoie la page contact
+     */
     @GetMapping("/contact")
     public String contact(Model model){
 
